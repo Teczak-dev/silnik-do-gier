ef1afc7deb73a3b6ede622024b25c81ac84ef353
Mikołaj Sobczak <teczaksupermiki@gmail.com>
2025-03-21 20:20:20 +0100

    setting up project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    auto lang = "C++";
+    cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        cout << "i = " << i << endl;
+    }
+
+    return 0;
+}
